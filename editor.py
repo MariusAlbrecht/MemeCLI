1feb3750811a6db895664ed5bc2354ca7a11e9e0
Marius <marius.albrecht@freenet.de>
2021-11-01 18:23:11 +0100

    draws arrays

--- editor.py
+++ editor.py
@@ -15,29 +15,31 @@ class drawTextOnImage():
         self.draw = ImageDraw.Draw(self.imagePath)
         self.font = ImageFont.truetype(fontPathName, self.fontSize)
 
-    def drawTextOnImage(self, x, y, text):
-        self.draw.text((10, 25), "world", font=self.font)
+    def drawTextOnImage(self, textList, xPosList, yPosList):
+        for text in textList:
+            for xPos in xPosList:
+                for yPos in yPosList:
+                    self.draw.text((xPos, yPos), text, font=self.font)
 
 class Profilehandler():
     def __init__(self, pathToJson = ""):
         self.pathToJson = pathToJson
         profiles = []
 
-    def addProfile(imagePath, textXposYposArray, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None):
-        newProfile = Profile(imagePath, textXposYposArray, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None)
-
-    def profileHandler(profileID = 0):
-        a
+    def addProfile(imagePath, textList, xPosList, yPosList, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None):
+        newProfile = Profile(imagePath, textList, xPosList, yPosList, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None)
 
 class Profile():
-    def __init__(self, imagePath, textXposYposArray, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None):
+    def __init__(self, imagePath, textList, xPosList, yPosList, fontFolderPath = "C:\Windows\Fonts\\", fontName = "arial", fontExtension = ".ttf", fontSize = 15, anchorposition = None):
 
         self.fontFolderPath = fontFolderPath
         self.fontName = fontName
         self.fontExtension = fontExtension          #font path defaults to "C:\Windows\Fonts\arial.ttf", where "C:\Windows\Fonts\" is the windows standrart directory for font files
         self.fontSize = fontSize                    #font size defaults to 15           
         self.imagePath = imagePath                  #image path has to be input
-        self.textXposYposArray = textXposYposArray  #3D Aray that holds text, and corresponding anchorpoints (with x and y pos)
+        self.textList = textList
+        self.xPosList = xPosList
+        self.yPosList = yPosList                    #List of y Posititons, corresponding with the same indexes on xPosList and textList to essentially make a 3d array holding text and anchorpoint
 
         profileArray = []
 
@@ -53,3 +55,4 @@ class Profile():
 
         with open("profiles.json", "w") as jsonFile:
             json.dump(profileArray[len(profileArray)], jsonFile)
+
